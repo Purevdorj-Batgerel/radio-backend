c31d556dc240da667ba9c447656cfea38f5c8db8
Purevdorj Batgerel <purvee.hq@gmail.com>
2024-06-09 14:41:33 -0500

    chore: update .env settings

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  if (!env.WWW_HOST) {
+    throw new Error('WWW_HOST not declared in the .env file')
+  }
+  if (!env.WWW_PORT) {
+    throw new Error('WWW_PORT not declared in the .env file')
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      host: env.WWW_HOST,
+      port: parseInt(env.WWW_PORT),
+    },
+  }
 })
